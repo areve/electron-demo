5dd96c2e5f675749817559e7a9db61bf2824d2bb
areve <areve@example.com>
2018-10-21 15:25:20 +0100

    tidied code a bit

--- main.js
+++ main.js
@@ -1,143 +1,116 @@
-const windowStateKeeper = require('electron-window-state');
 
-if (require('electron-squirrel-startup')) return;
+if (require('electron-squirrel-startup')) return
 
-const path = require('path')
-const { app, BrowserWindow, Tray, Menu } = require('electron')
+if (handleSquirrelEvent()) return
 
-if (handleSquirrelEvent()) {
-  // squirrel event handled and app will exit in 1000ms, so don't do anything else
-  return;
-}
+initialize()
+
 function handleSquirrelEvent() {
-  if (process.argv.length === 1) {
-    return false;
-  }
+  if (process.argv.length === 1) return false
+  const path = require('path')
+  const ChildProcess = require('child_process')
+  const exeName = path.basename(process.execPath)
+  const squirrelEvent = process.argv[1]
 
-  const ChildProcess = require('child_process');
-  const path = require('path');
-
-  const appFolder = path.resolve(process.execPath, '..');
-  const rootAtomFolder = path.resolve(appFolder, '..');
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-  const exeName = path.basename(process.execPath);
-
-  const spawn = function (command, args) {
-    let spawnedProcess, error;
-
-    try {
-      spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
-    } catch (error) { }
-
-    return spawnedProcess;
-  };
-
-  const spawnUpdate = function (args) {
-    return spawn(updateDotExe, args);
-  };
-
-  const squirrelEvent = process.argv[1];
   switch (squirrelEvent) {
     case '--squirrel-install':
     case '--squirrel-updated':
-      // Optionally do things such as:
-      // - Add your .exe to the PATH
-      // - Write to the registry for things like file associations and
-      //   explorer context menus
-
-      // Install desktop and start menu shortcuts
-      spawnUpdate(['--createShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
+      spawnUpdate(['--createShortcut', exeName])
+      setTimeout(app.quit, 1000)
+      return true
     case '--squirrel-uninstall':
-      // Undo anything you did in the --squirrel-install and
-      // --squirrel-updated handlers
-
-      // Remove desktop and start menu shortcuts
-      spawnUpdate(['--removeShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
+      spawnUpdate(['--removeShortcut', exeName])
+      setTimeout(app.quit, 1000)
+      return true
     case '--squirrel-obsolete':
-      // This is called on the outgoing version of your app before
-      // we update to the new version - it's the opposite of
-      // --squirrel-updated
-
-      app.quit();
-      return true;
+      app.quit()
+      return true
   }
-};
 
-let win
-let tray = null
+  function spawn(command, args) {
+    let spawnedProcess, error
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, { detached: true })
+    } catch (error) { }
+    return spawnedProcess
+  }
 
-function createWindow() {
-  tray = new Tray(path.resolve(__dirname, 'favicon.ico'))
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Exit',
-      click() {
-        win.closeEnabled = true
-        win.close()
-
-      }
-    },
-    {
-      label: 'Debug',
-      click() {
-        win.webContents.openDevTools()
-      }
-    },
-  ])
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
-
-  let mainWindowState = windowStateKeeper({
-    defaultWidth: 1000,
-    defaultHeight: 800
-  });
-
-  win = new BrowserWindow({
-    show: false,
-    'x': mainWindowState.x,
-    'y': mainWindowState.y,
-    'width': mainWindowState.width,
-    'height': mainWindowState.height,
-    fullscreen: false,
-    alwaysOnTop: false,
-    backgroundColor: '#ccc',
-    frame: false,
-    webPreferences: {
-      devTools: true
-    }
-  })
-
-  mainWindowState.manage(win);
-
-
-  win.once('ready-to-show', () => {
-    win.show()
-  })
-  win.loadFile('index.html')
-  win.setMenu(null)
-  win.closeEnabled = false
-  win.on('close', (event) => {
-    if (!win.closeEnabled) event.preventDefault()
-  })
-  win.on('closed', () => { win = null })
+  function spawnUpdate(args) {
+    const appFolder = path.resolve(process.execPath, '..')
+    const rootAtomFolder = path.resolve(appFolder, '..')
+    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'))
+    return spawn(updateDotExe, args)
+  }
 }
 
-app.on('ready', createWindow)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+function initialize() {
+  const { app, BrowserWindow, Tray, Menu } = require('electron')
+  const windowStateKeeper = require('electron-window-state')
+  const path = require('path')
 
-app.on('activate', () => {
-  if (win === null) createWindow()
-})
+  let win = null
 
+  app.on('ready', initialize)
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
 
+  app.on('activate', () => {
+    if (win === null) initialize()
+  })
+
+  function initialize() {
+    createTrayIcon()
+    createWindow()
+  }
+
+  function createTrayIcon() {
+    const tray = new Tray(path.resolve(__dirname, 'favicon.ico'))
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'Exit',
+        click() {
+          win.closeEnabled = true
+          win.close()
+        }
+      },
+      {
+        label: 'Debug',
+        click() {
+          win.webContents.openDevTools()
+        }
+      },
+    ])
+
+    tray.setToolTip('electron-pwa-demo')
+    tray.setContextMenu(contextMenu)
+  }
+
+  function createWindow() {
+    let mainWindowState = windowStateKeeper({
+      defaultWidth: 1000,
+      defaultHeight: 800
+    })
+
+    win = new BrowserWindow({
+      show: false,
+      'x': mainWindowState.x,
+      'y': mainWindowState.y,
+      'width': mainWindowState.width,
+      'height': mainWindowState.height,
+      frame: false,
+      webPreferences: { devTools: true }
+    })
+
+    mainWindowState.manage(win)
+    win.once('ready-to-show', () => win.show())
+    win.loadFile('index.html')
+    win.setMenu(null)
+    win.closeEnabled = false
+    win.on('close', (event) => {
+      if (!win.closeEnabled) event.preventDefault()
+    })
+    win.on('closed', () => { win = null })
+  }
+}
 
