52af589c06569169578db2253194f2e55d972dcc
areve <areve@example.com>
2018-10-21 18:31:52 +0100

    attempt to pre cache

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -3,6 +3,10 @@
 var CACHE = 'pwabuilder-precache';
 var precacheFiles = [
       /* Add an array of files to precache for your app */
+      'index.html',
+      'manifest.json',
+      'page.js',
+      'manin.css'
     ];
 
 //Install stage sets up the cache-array to configure pre-cache content
